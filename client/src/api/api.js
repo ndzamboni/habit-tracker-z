fd161827e358feb13cc713cd7df643e86804dce3
ndzamboni <ndzamboni@gmail.com>
2024-06-22 19:37:35 -0400

    fresh start

--- client/src/api/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_BASE_URL = 'http://localhost:5000/api';
-
-export const createUser = (userData) => axios.post(`${API_BASE_URL}/users`, userData);
-export const getUser = (userId) => axios.get(`${API_BASE_URL}/users/${userId}`);
-export const createHabit = (habitData) => axios.post(`${API_BASE_URL}/habits`, habitData);
-export const getHabit = (habitId) => axios.get(`${API_BASE_URL}/habits/${habitId}`);
--- src/routes/habitRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const habitController = require('../controllers/habitController');
-
-router.post('/', habitController.createHabit);
-router.get('/:id', habitController.getHabit);
-
-module.exports = router;

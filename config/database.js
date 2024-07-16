e0ac783840c1f2cca792b3160ac15b007070bf4e
ndzamboni <ndzamboni@gmail.com>
2024-07-15 23:05:42 -0400

    update database.js

--- config/database.js
+++ config/database.js
@@ -1,11 +1,9 @@
 const { Pool } = require('pg');
-const config = require('./config');
+require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: config.databaseUrl,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
